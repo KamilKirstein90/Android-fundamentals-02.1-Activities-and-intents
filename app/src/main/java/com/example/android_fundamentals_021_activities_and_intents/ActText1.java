7401a099de1dea30f1dba1a687079102e646e7c3
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-28 12:21:48 +0200

    [AF021] Buttons,  method for intents
    Created thre buttons and the function to get per button click to wished activity per intent.

---
+++ app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/ActText1.java
@@ -0,0 +1,14 @@
+package com.example.android_fundamentals_021_activities_and_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActText1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_text1);
+    }
+}
--- app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/MainActivity.java
+++ app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.android_fundamentals_021_activities_and_intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void goToText(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.btn_text1:
+                openNewActivity(this, ActText1.class);
+                break;
+
+        }
+    }
+
+    public void openNewActivity(Context ct, Class <?> act){
+
+        Intent intent = new Intent(ct, act);
+        startActivity(intent);
+
     }
 }
