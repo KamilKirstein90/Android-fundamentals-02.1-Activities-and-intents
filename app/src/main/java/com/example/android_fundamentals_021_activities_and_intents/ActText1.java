76c6ffbbe87838e020b372a488be494fa918e4f8
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-28 18:30:42 +0200

    [AF021]  Created the second act for the intent
    Secound activity have been created that is started with an intent in the openNewActivityWithNewText method. For every different button click in the goToText method the method openNewActivityWithNewText with a different text is called.
    The text is fetched from the strings.xml with the right id. In the ActText1 activity the intent is fetched with getIntetn() and the extra is extracted from it. With getStringExtra and the key final static String EXTRA_TEXT from MainActivity. The text is then passed to the text view in the Scroll View of ActText1.

--- app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/ActText1.java
+++ app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/ActText1.java
@@ -2,13 +2,26 @@ package com.example.android_fundamentals_021_activities_and_intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class ActText1 extends AppCompatActivity {
+
+    TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_act_text1);
+        tv_text = findViewById(R.id.tV_text);
+
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        tv_text.setText(text);
+
     }
 }
--- app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/MainActivity.java
+++ app/src/main/java/com/example/android_fundamentals_021_activities_and_intents/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    final static String EXTRA_TEXT = "MainActivity.extras.text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
     }
 
     public void goToText(View view) {
 
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.btn_text1:
-                openNewActivity(this, ActText1.class);
+                openNewActivityWithNewText(this, ActText1.class, EXTRA_TEXT, getResources().getString(R.string.text1));
                 break;
 
+            case R.id.btn_text2:
+                openNewActivityWithNewText(this, ActText1.class, EXTRA_TEXT, getResources().getString(R.string.text2));
+                break;
+
+            case R.id.btn_text3:
+                openNewActivityWithNewText(this, ActText1.class, EXTRA_TEXT, getResources().getString(R.string.text3));
+                break;
         }
     }
 
-    public void openNewActivity(Context ct, Class <?> act){
+    public void openNewActivityWithNewText(Context ct, Class<?> act, String extraKey, String text) {
 
         Intent intent = new Intent(ct, act);
+        intent.putExtra(extraKey, text);
         startActivity(intent);
-
     }
 }
